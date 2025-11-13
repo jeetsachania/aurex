b5773eb7224c4ba4356eb41f4a504c5436971523
jeetsachania <jeetsachania.business@gmail.com>
2025-11-13 17:44:34 +0000

    Update CSS imports.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,34 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import "./styles/Global.css";
-import "./styles/Fonts.css";
+import "./styles/Typography.css";
+
+import "./styles/components/Navbar.css";
+import "./styles/components/Footer.css";
+import "./styles/components/Form.css";
+import "./styles/components/Button.css";
+import "./styles/components/Card.css";
+import "./styles/components/List.css";
+
+import "./styles/pages/Pricing.css";
+import "./styles/pages/Home.css";
+import "./styles/pages/Login.css";
 
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/features" element={<Index scrollTo="features" />} />
-        <Route path="/testimonials" element={<Index scrollTo="testimonials" />} />
         <Route path="/pricing" element={<Index scrollTo="pricing" />} />
         <Route path="/faq" element={<Index scrollTo="faq" />} />
         <Route path="/login" element={<Login />} />
