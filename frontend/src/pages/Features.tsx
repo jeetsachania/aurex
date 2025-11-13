3b9018834b1ee6ae4af1816dbf842bc41548bd64
jeetsachania <jeetsachania.business@gmail.com>
2025-11-13 17:42:40 +0000

    Add feature content.

--- frontend/src/pages/Features.tsx
+++ frontend/src/pages/Features.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const Features: React.FC = () => {
   return (
-    <div className='container-fluid default-container' id='features'>
-      <h1>Features</h1>
+    <div className="container-fluid">
+      <section className="container my-5">
+        <div className="row align-items-center">
+          <div className="col-md-6">
+            <h2 className="custom-header mb-4">Power At Your Fingertips</h2>
+            <ul className="list-unstyled features-list">
+              <li className="mb-3 d-flex align-items-start">
+                <i className="bi bi-check me-2"></i>
+                <span>Real-Time Market Data</span>
+              </li>
+              <li className="mb-3 d-flex align-items-start">
+                <i className="bi bi-check me-2"></i>
+                <span>Secure Transactions</span>
+              </li>
+              <li className="mb-3 d-flex align-items-start">
+                <i className="bi bi-check me-2"></i>
+                <span>Portfolio Analysis</span>
+              </li>
+              <li className="mb-3 d-flex align-items-start">
+                <i className="bi bi-check me-2"></i>
+                <span>Custom Alerts</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
