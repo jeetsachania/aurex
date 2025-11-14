53aef3f6813b707984c48641dc4ec8d6b3b0358f
jeetsachania <jeetsachania.business@gmail.com>
2025-11-14 17:31:03 +0000

    Support firstname and lastname attributes for new users.

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import Logo from "../assets/svgs/Logo";
 
-import { validateEmail, validateUsername, validatePassword } from "../utils/Utils";
+import {
+  validateEmail,
+  validateUsername,
+  validatePassword,
+} from "../utils/Utils";
 
 const Signup: React.FC = () => {
   const navigate = useNavigate();
+  const [firstname, setFirstname] = useState<string>("");
+  const [lastname, setLastname] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [confirmEmail, setConfirmEmail] = useState<string>("");
   const [username, setUsername] = useState<string>("");
@@ -18,22 +25,47 @@ const Signup: React.FC = () => {
     return email === confirmEmail;
   }
 
-  const handlFormSubmit = (event: React.FormEvent) => {
+  const handlFormSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     if (!validateEmail(email)) {
-      alert("email error")
+      alert("email error");
     }
 
     if (!validateUsername(username)) {
-      alert("username error")
+      alert("username error");
     }
 
     if (!emailsMatch(email, confirmEmail)) {
-      alert("emails do not match")
+      alert("emails do not match");
     }
 
-    if (!validatePassword(password) || !validatePassword(confirmPassword) || !(password === confirmPassword)) {
-      alert("passwords do not match")
+    if (
+      !validatePassword(password) ||
+      !validatePassword(confirmPassword) ||
+      !(password === confirmPassword)
+    ) {
+      alert("passwords do not match");
+    }
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/users/register",
+        {
+          email,
+          firstname,
+          lastname,
+          username,
+          password,
+        }
+      );
+      navigate("/login");
+    } catch (error) {
+      if (error.response) {
+        alert("Error registering: " + error.response.data.detail);
+      } else {
+        console.error(error);
+        alert("Error registering. Please try again later.");
+      }
     }
   };
 
@@ -53,6 +85,44 @@ const Signup: React.FC = () => {
             <Logo className="logo" />
             <h2 className="card-title custom-form-card-title">Signup</h2>
             <form onSubmit={handlFormSubmit}>
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <div className="input-group">
+                    <div className="input-group-append">
+                      <span className="input-group-text input-group-icon">
+                        <i className="bi bi-envelope-fill"></i>
+                      </span>
+                    </div>
+                    <input
+                      type="text"
+                      className="form-control custom-form-input"
+                      id="firstname"
+                      placeholder="First Name"
+                      value={firstname}
+                      onChange={(e) => setFirstname(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6 mb-3">
+                  <div className="input-group">
+                    <div className="input-group-append">
+                      <span className="input-group-text input-group-icon">
+                        <i className="bi bi-envelope-fill"></i>
+                      </span>
+                    </div>
+                    <input
+                      type="text"
+                      className="form-control custom-form-input"
+                      id="lastname"
+                      placeholder="Last Name"
+                      value={lastname}
+                      onChange={(e) => setLastname(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
               <div className="row">
                 <div className="col-md-6 mb-3">
                   <div className="input-group">
