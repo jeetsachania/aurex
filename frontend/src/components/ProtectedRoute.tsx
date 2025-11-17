67bbd1e8d8020615db54e64cebb47ec96ea7c349
jeetsachania <jeetsachania.business@gmail.com>
2025-11-17 15:58:00 +0000

    Support protected routes to allow access only when logged in.

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,8 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+const ProtectedRoute = () => {
+    const accessToken = localStorage.getItem("access_token");
+    return accessToken ? <Outlet /> : <Navigate to="/login" />
+}
+
+export default ProtectedRoute;
