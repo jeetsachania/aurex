c0419d9b501f970b6b44bff4b17360703372d2fb
jeetsachania <jeetsachania.business@gmail.com>
2026-01-28 19:41:52 +0000

    Add relationship between user and wallet

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from passlib.context import CryptContext
 
 from app.db.database import Base
@@ -10,24 +11,31 @@ class User(Base):
     User Model
 
     Attributes:
-        id (int): Auto-incremented unique ID.
-        firstname (str): User's firstname.
-        lastname (str): User's lastname.
-        email (str): User's email address.
-        username (str): User's username.
-        hashed_password (str): User's password.
+        id (`int`): Auto-incremented unique ID.
+        firstname (`str`): User's firstname.
+        lastname (`str`): User's lastname.
+        email (`str`): User's email address.
+        username (`str`): User's username.
+        hashed_password (`str`): User's password.
 
     Methods:
         `verify_password(password: str) -> bool`:
             Verifies the provided password matches the stored hash password.
     """
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
     firstname = Column(String, unique=False, index=True)
     lastname = Column(String, unique=False, index=True)
     email = Column(String, unique=True, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+    wallets = relationship(
+        "Wallet",
+        back_populates="user",
+        cascade="all, delete-orphan",
+    )
     
     def verify_password(self, password: str) -> bool:
         """
