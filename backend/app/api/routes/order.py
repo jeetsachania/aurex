8c728721c895e16859459843716503b4e418a1dc
jeetsachania <jeetsachania.business@gmail.com>
2026-02-27 11:47:34 +0000

    Check user wallet balance when placing an order

--- backend/app/api/routes/order.py
+++ backend/app/api/routes/order.py
@@ -1,20 +1,24 @@
+from decimal import Decimal
 from typing import List
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.api.deps import get_current_user
-from app.crud.utils import commit, get_all
+from app.crud.utils import commit, get, get_and_lock, get_all
 from app.db.database import get_db
+from app.models.asset import Asset
 from app.models.user import User
 from app.models.order import Order
+from app.models.wallet import Wallet
 from app.schemas.order import OrderCreate, OrderResponse
 
 
 router = APIRouter()
 
 
-@router.post("/", response_model=OrderResponse, status_code=status.HTTP_200_OK)
+@router.post("/", response_model=OrderResponse, status_code=status.HTTP_201_CREATED)
 def create(order: OrderCreate, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     """
     Add a new order to the database
@@ -25,8 +29,34 @@ def create(order: OrderCreate, user: User = Depends(get_current_user), db: Sessi
         db (`Session`): SQLAlchemy database session
 
     Returns:
-        `Order`: The Order object
+        `Order`: Order object
     """
+    asset = get(db, Asset, symbol=order.asset)
+
+    if not asset:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Asset '{order.asset}' not found"
+        )
+
+    wallet = get_and_lock(db, Wallet, user_id=user.id, currency=asset.currency)
+
+    if not wallet:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"{asset.currency} wallet required"
+        )
+
+    required_margin: Decimal = order.quantity * order.price
+
+    if required_margin > wallet.balance:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Insufficient balance"
+        )
+
+    wallet.balance -= required_margin
+
     new_order = Order(
         user_id=user.id,
         asset_type=order.asset_type,
@@ -34,14 +64,14 @@ def create(order: OrderCreate, user: User = Depends(get_current_user), db: Sessi
         quantity=order.quantity,
         price=order.price,
         order_type="BUY",
-        order_status="PENDING"
+        order_status="PENDING",
     )
 
     return commit(db, new_order)
 
 
 @router.get("/", response_model=List[OrderResponse], status_code=status.HTTP_200_OK)
-def get(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+def get_all_orders(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     """
     Get all orders
 
@@ -52,4 +82,4 @@ def get(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     Returns:
         orders (`list[Order]`): List of all Order objects
     """
-    return get_all(db, Order, message="No orders found")
+    return get_all(db, Order)
